ccd6c88b16a574e18ddc984d913b1296d05a9f87
Kalle <kalle.stalhandske@gmail.com>
2026-06-09 11:33:22 +0200

    Added click and drop function

--- frontend/src/components/chess/board/ChessBoard.tsx
+++ frontend/src/components/chess/board/ChessBoard.tsx
@@ -1,19 +1,63 @@
+"use client";
+
+import { useState } from "react";
 import { Square } from "./Square";
 import { initialBoard } from "@/lib/chess/initialBoard";
 
 export function ChessBoard() {
+  const [board, setBoard] = useState(initialBoard);
+  const [selectedSquare, setSelectedSquare] = useState<number | null>(null);
+
+  function handleSquareClick(index: number) {
+    const clickedPiece = board[index];
+
+    if (selectedSquare === null) {
+      // No square selected
+      if (clickedPiece) {
+        setSelectedSquare(index);
+      }
+
+      return;
+    }
+
+    if (selectedSquare === index) {
+      // Same square was selected, deactivate
+      setSelectedSquare(null);
+      return;
+    }
+
+    setBoard((currentBoard) => {
+      const newBoard = [...currentBoard];
+      const movingPiece = newBoard[selectedSquare];
+      if (!movingPiece) {
+        return currentBoard;
+      }
+
+      newBoard[index] = movingPiece;
+      newBoard[selectedSquare] = null;
+
+      return newBoard;
+    })
+    setSelectedSquare(null);
+  }
+
+
   return (
     <div className="grid grid-cols-8 grid-rows-8 w-[min(90vw,640px)] aspect-square border overflow-hidden">
-      {initialBoard.map((piece, index) => {
+      {board.map((piece, index) => {
         const row = Math.floor(index / 8);
         const col = index % 8;
         const isLight = (row + col) % 2 === 0;
+        const isSelected = selectedSquare === index;
 
         return (
           <Square
             key={index}
             square={piece}
+            index={index}
             isLight={isLight}
+            isSelected={isSelected}
+            onClick={handleSquareClick}
           />
         );
       })}
--- frontend/src/components/chess/board/Square.tsx
+++ frontend/src/components/chess/board/Square.tsx
@@ -1,18 +1,26 @@
+"use client";
+
 import { Piece } from "./Piece";
 import type { Square as SquareData } from "@/lib/chess/types";
 
 type SquareProps = {
   square: SquareData;
+  index: number;
   isLight: boolean;
+  isSelected: boolean;
+  onClick: (index: number) => void;
 };
 
-export function Square({ square, isLight }: SquareProps) {
+
+export function Square({ square, index, isLight, isSelected, onClick }: SquareProps) {
   return (
     <div
+      onClick={() => onClick(index)}
       className={`
         relative aspect-square min-w-0 min-h-0
         flex items-center justify-center overflow-hidden
         ${isLight ? "bg-stone-200" : "bg-stone-700"}
+        ${isSelected ? "ring-4 ring-yellow-400 ring-inset" : ""}
       `}
     >
       {square && <Piece piece={square} />}
